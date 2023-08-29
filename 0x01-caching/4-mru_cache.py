c5f5fa7c20d6d8cc6154377721752e8c681b0fc5
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-08-29 12:30:29 +0100

    In 4-mru_cache from line 17 to MRUCache(BaseCaching): was Modified.

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -11,17 +11,17 @@ class MRUCache(BaseCaching):
     """
         MRUCache
     """
+
     def __init__(self):
         super().__init__()
         self.history = []
 
     def put(self, key, item):
-        """ 
-            assigns the new item to the dictionary
+        """assigns the new item to the dictionary
         """
         if not (key is None or item is None):
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
-                 and key not in self.cache_data:
+                    and key not in self.cache_data:
                 print(f'DISCARD: {self.history[-1]}')
                 self.cache_data.pop(self.history[-1])
                 del self.history[-1]
@@ -31,8 +31,7 @@ class MRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ 
-            returns the value in self.cache_data linked to key
+        """returns the value in self.cache_data linked to key
         """
         if key is None or not (key in self.cache_data):
             return None
@@ -40,4 +39,3 @@ class MRUCache(BaseCaching):
             del self.history[self.history.index(key)]
             self.history.append(key)
             return self.cache_data[key]
-
