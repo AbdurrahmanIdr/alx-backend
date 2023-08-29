00880145334baa24a934fb17ccc45a707d150b8d
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-08-29 12:08:49 +0100

    In 4-mru_cache from line 5 to 6 was Modified.

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" MRU caching
+"""
+    MRU caching
 """
 
 
@@ -7,14 +8,16 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache
+    """
+        MRUCache
     """
     def __init__(self):
         super().__init__()
         self.history = []
 
     def put(self, key, item):
-        """ assigns the new item to the dictionary
+        """ 
+            assigns the new item to the dictionary
         """
         if not (key is None or item is None):
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
@@ -28,7 +31,8 @@ class MRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ returns the value in self.cache_data linked to key
+        """ 
+            returns the value in self.cache_data linked to key
         """
         if key is None or not (key in self.cache_data):
             return None
