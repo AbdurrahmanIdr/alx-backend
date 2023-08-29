329b173a9daa2e3a00c61e9d6893ca1a78a0a933
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-08-29 12:30:27 +0100

    In 1-fifo_cache from line 20 to 18 was Modified.

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-""" 
-    FIFO caching
+"""FIFO caching
 """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ 
-        FIFOCache
+    """FIFOCache
     """
+
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
-        """ 
-            assigns the new item to the dictionary
+        """assigns the new item to the dictionary
         """
         if not (key is None or item is None):
             self.cache_data.update({key: item})
@@ -24,10 +22,8 @@ class FIFOCache(BaseCaching):
                 print(f'DISCARD: {first_in}')
 
     def get(self, key):
-        """ 
-            returns the value in self.cache_data linked to key
+        """returns the value in self.cache_data linked to key
         """
         if key is None or not (key in self.cache_data):
             return None
         return self.cache_data[key]
-
