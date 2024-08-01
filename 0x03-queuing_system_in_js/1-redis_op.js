324071adb1bc7536f925c1dd4239cd25ae6620a1
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-08-02 00:19:48 +0100

    feat: add setNewSchool function

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/**
+ * Connect to redis server via redis client
+ */
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.SET(schoolName, value, print);
+}
+
