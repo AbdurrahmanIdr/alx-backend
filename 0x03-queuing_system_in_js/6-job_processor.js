f237879b7f8e02a73b6155fd101ff2e5a930b6c4
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-08-02 00:27:38 +0100

    feat: add error handling to job processor

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -10,3 +10,7 @@ function sendNotification(phoneNumber, message) {
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
 
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
